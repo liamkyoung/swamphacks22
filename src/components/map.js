68ee9198411674d7ccfb71beb9f172463a536daa
Hunter Lawson <hunterlaw9son@gmail.com>
2022-01-29 18:14:10 -0500

    add custom marker

--- src/components/map.js
+++ src/components/map.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import L from 'leaflet';
+import { divIcon, L } from 'leaflet';
 import BottomPopUp from '../components/bottompopup'
 import AddEventButton from './addeventbutton'
 
-const icon = L.icon({ iconUrl: "/images/marker-icon.png" });
-
 const Map = () => {
-  const position = [29.649, -82.344]
+    const position = [29.649, -82.344]
+    const icon = divIcon({
+        className: "",
+        html: '<div id="map-anchor">💡</div>'
+    });
+
+    function testPrint(e) {
+        console.log(e);
+    }
+
     return (
         <div className='relative h-auto w-screen'>
             <MapContainer className='mapContainer absolute left-0 top-0 z-0' center={position} zoom={20} scrollWheelZoom={true}>
@@ -16,10 +23,9 @@ const Map = () => {
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position} icon={icon}>
-                    <Popup>
-                        <BottomPopUp />
-                    </Popup>
+                <Marker position={position} icon={icon}
+                    eventHandlers={{ click: testPrint }}
+                >
                 </Marker>
             </MapContainer>
             <div className='absolute bottom-10 right-5 z-100 h-16 w-16 cursor-pointer text-ORANGE hover:text-BLUE rounded-full'>
