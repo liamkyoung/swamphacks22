9a3185566c49594ad260d4ccef85c59e4cb54dd8
Kingston <liamkingstonyoung@gmail.com>
2022-01-29 14:55:28 -0500

    Fixing UI stuff

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,25 +1,67 @@
-import { useState } from 'react'
-import { Listbox } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 
 function Dropdown ({ items }) {
-  const [selectedItem, setSelectedItem] = useState(items[0])
+  const [selected, setSelected] = useState(items[0])
 
   return (
-    <Listbox value={selectedItem} onChange={setSelectedItem}>
-      <Listbox.Button>{selectedItem.name}</Listbox.Button>
-      <Listbox.Options>
-        {items.map((item) => (
-          <Listbox.Option
-            key={item.id}
-            value={item.name}
-            disabled={item.unavailable}
-            className={'text-xl'}
+    <div className="w-auto top-16">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+            <span className="block truncate">{selected.name}</span>
+            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+              <SelectorIcon
+                className="w-5 h-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            {item.name}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
-    </Listbox>
+            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm text-center">
+              {items.map((item, itemId) => (
+                <Listbox.Option
+                  key={itemId}
+                  className={({ active }) =>
+                    `${active ? 'text-white bg-BLUE' : 'text-gray-900'}
+                          cursor-default select-none relative py-2 pl-10 pr-4`
+                  }
+                  value={item}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`${
+                          selected ? 'font-medium' : 'font-normal'
+                        } block truncate`}
+                      >
+                        {item.name}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`${
+                            active ? 'text-amber-600' : 'text-amber-600'
+                          }
+                                absolute inset-y-0 left-0 flex items-center pl-3`}
+                        >
+                          <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
   )
 }
 
