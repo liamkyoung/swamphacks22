2bb3cef46ae003170bf2da5b276491359c6486c1
Andrew Ruales <aruales@Andrews-MacBook-Pro.local>
2022-01-29 23:38:44 -0500

    lol some ui

--- src/components/addeventbutton.js
+++ src/components/addeventbutton.js
@@ -2,9 +2,16 @@ import React from 'react'
 import { PlusCircleIcon } from '@heroicons/react/solid';
 import { useState } from "react";
 import Modal from "../components/modal"
+import TextField from '@material-ui/core/TextField';
 
 function AddEventButton() {
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('You have submitted the form.')
+  }
   const [showModal, setShowModal] = useState(false);
+  var today = new Date();
+  today = today.getDate();
   return (
     <div className=''>
       <button onClick={() => setShowModal(true)} className="btn">
@@ -12,12 +19,63 @@ function AddEventButton() {
       </button>
 
       <Modal show={showModal} onClose={() => setShowModal(false)}>
-        <form>
-          <label for="fname">First name:</label><br />
-          <input className='rounded-lg bg-GRAY' type="text" id="fname" name="fname" /><br />
-          <label for="lname">Last name:</label><br />
-          <input className='rounded-lg bg-GRAY' type="text" id="lname" name="lname" />
-        </form>
+        <div className='flex flex-col items-center'>
+          <h1 className='text-center text-3xl'>Event Form</h1>
+          <form onSubmit={handleSubmit}>
+            <label for="org">Organization Name:</label><br />
+            <input className='rounded-lg bg-GRAY' type="text" id="org" name="org" /><br />
+            <label for="event">Event Name:</label><br />
+            <input className='rounded-lg bg-GRAY' type="text" id='event' name='event' /><br />
+            <label for="descrip">Event Description:</label><br />
+            <input className='rounded-lg bg-GRAY' type="text" id='descrip' name='descrip' /><br />
+            <TextField
+            id="date"
+            label="Start Date"
+            type="date"
+            defaultValue= {today}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            /><br />
+            {/* <input className='rounded-lg bg-GRAY' type="text" id='stime' name='stime' /><br />
+            <label for="start">Start Time:</label><br /> */}
+            <TextField
+            label="Start Time"
+            defaultValue="12:00"
+            type="time"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            // 5 minutes
+            inputProps={{
+              step: 60,
+            }}
+            /><br />
+            <TextField
+            id="date"
+            label="End Date"
+            type="date"
+            defaultValue= {today}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            /><br />
+            <TextField
+            label="End Time"
+            defaultValue="12:00"
+            type="time"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            // 5 minutes
+            inputProps={{
+              step: 60,
+            }}
+            /><br />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        
       </Modal>
     </div>
   )
